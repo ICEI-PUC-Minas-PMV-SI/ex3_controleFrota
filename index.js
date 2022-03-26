8608d18e207ac7fdf3ba283e628dc7ecd9a5ebd1
Cleverson Rocha <81713293+cleversoncr@users.noreply.github.com>
2022-03-26 20:10:27 -0300

    Troca para porta 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { connectDb, disconnectDb } = require('./models/Database');
 const Car = require('./models/Car-model')
 const app = express()
-const port = 3000
+const port = 8080
 app.use(express.json())
 
 app.use(
